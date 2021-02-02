b3de3828087bec095cfbc4485c7bf1266b89e994
Spencer Christy <spenrad@gmail.com>
2021-02-02 01:57:05 -0800

    seeds created for book_db

---
+++ db/seeds.sql
@@ -0,0 +1,5 @@
+INSERT INTO books (title, author, read) VALUES ('Growth of the Soil', 'Knut Hamsun', true);
+INSERT INTO books (title, author, read) VALUES ('Nausea', 'Paul Sartre', true);
+INSERT INTO books (title, author, read) VALUES ('No Longer Human', 'Osamu Dazai', true);
+INSERT INTO books (title, author, read) VALUES ('Never Let Me Go', 'Kazuo Ishiguro' true);
+INSERT INTO books (title, author) VALUES ('The Left Hand of Darkness', 'Ursula K. Le Guin');
