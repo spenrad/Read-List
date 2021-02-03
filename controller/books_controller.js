70ce838a25befb032811d63a56e6e53334dd43e0
Spencer Christy <spenrad@gmail.com>
2021-02-02 20:58:16 -0800

    added methods in book controller

--- controller/books_controller.js
+++ controller/books_controller.js
@@ -4,17 +4,34 @@ const router = express.Router();
 const book = require('../models/book.js');
 
 router.get('/', function (req, res) {
-    book.selectAll()
+    book.selectAll( function(data) {
+        const hbsObject = {
+            books : data
+        };
+        res.render('inndex', hbsObject);
+    })
 });
 
 router.post('/api/books', function (req, res){
-    book.insertOne()
+    book.insertOne(['titleAuth'], [req.body.titleAuth], function (result) {
+        res.json({ id: result.insertId });
+    })
 });
 
 router.put('/api/books/:id', function (req, res){
     const conditon = `id = ${req.params.id}`;
 
-    book.updateOne()
+    book.updateOnes(
+        {
+          finished : req.body.finished
+        },
+        condition, function(result) {
+            if(result.changedRows === 0) {
+                return res.status(404).end();
+            }
+            res.status(200).end();
+        }
+    )
 });
 
 module.exports = router;
